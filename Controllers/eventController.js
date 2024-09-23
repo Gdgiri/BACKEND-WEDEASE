93a98e3bc3a6a77f98b70ff17a3f949c87b7f3bb
giridharan s <160309516+gdharan361@users.noreply.github.com>
2024-09-23 21:50:25 +0530

    update edit and delete success

--- Controllers/eventController.js
+++ Controllers/eventController.js
@@ -25,11 +25,78 @@ export const fetchEvent = async (req, res) => {
     const getEvent = await Event.find();
     res
       .status(200)
-      .json({ message: "events fetch successfully", result: getEvent });
+      .json({ message: "event's fetch successfully", result: getEvent });
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: "event data fetch failure due to an internal server error",
+      message: "event data's fetch failure due to an internal server error",
+    });
+  }
+};
+
+// update
+
+export const updateEvent = async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(id);
+
+    const modifyEvent = await Event.findByIdAndUpdate(
+      id,
+      {
+        venueImg: req.body.venueImg,
+        venueName: req.body.venueName,
+        venuePlace: req.body.venuePlace,
+        venueAmount: req.body.venueAmount,
+        cateringName: req.body.cateringName,
+        cateringAmount: req.body.cateringAmount,
+        photographerName: req.body.photographerName,
+        photographerAmount: req.body.photographerAmount,
+        eventStylistName: req.body.eventStylistName,
+        eventStylistAmount: req.body.eventStylistAmount,
+        entertainerName: req.body.entertainerName,
+        entertainerAmount: req.body.entertainerAmount,
+        beauticianName: req.body.beauticianName,
+        beauticianAmount: req.body.beauticianAmount,
+        transportName: req.body.transportName,
+        transportAmount: req.body.transportAmount,
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!modifyEvent) {
+      return res.status(404).json({ message: "event not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "event updated successfully", result: modifyEvent });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "events update failure due to internal server error" });
+  }
+};
+
+// delete
+
+export const deleteEvent = async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(id);
+
+    const removeEvent = await Event.findByIdAndDelete(id);
+
+    if (!removeEvent) {
+      return res.status(404).json({ message: "event not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "event deleted successfully", result: removeEvent });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "event data failed to delete due to internal server error",
     });
   }
 };
--- Routes/eventRouters.js
+++ Routes/eventRouters.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { eventCreate, fetchEvent } from "../Controllers/eventController.js";
+import {
+  deleteEvent,
+  eventCreate,
+  fetchEvent,
+  updateEvent,
+} from "../Controllers/eventController.js";
 
 const router = express.Router();
 
 router.post("/create", eventCreate);
 router.get("/getevent", fetchEvent);
+router.put("/editevent/:id", updateEvent);
+router.delete("/delete/:id", deleteEvent);
 
 export default router;
